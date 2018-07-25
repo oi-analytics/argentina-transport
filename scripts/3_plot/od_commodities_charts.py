016fe928a229139118679add177276734c87a4bc
Tom Russell <tomalrussell@gmail.com>
2018-07-25 15:50:49 +0100

    Plot od commodities

---
+++ scripts/3_plot/od_commodities_charts.py
@@ -0,0 +1,31 @@
+"""Plot commodities matrices
+"""
+import os
+
+import cartopy.crs as ccrs
+import pandas
+
+from oia.utils import load_config
+
+
+def main(config):
+    """Read data, plot charts
+    """
+    data_path = config['paths']['data']
+    od_file = os.path.join(data_path, 'usage', 'economic_od.csv')
+    od = pandas.read_csv(od_file)
+
+    # TODO extend from notes to proof of concept
+
+    # integer zone ids
+    od.from_zone = od.from_zone.apply(lambda z: int(z.replace("econ_", "")))
+    od.to_zone = od.to_zone.apply(lambda z: int(z.replace("econ_", "")))
+
+    # pivot and plot
+    mat = od[od.sector == 'meat'].drop('sector', axis=1).pivot("from_zone", "to_zone", "value")
+    ax = seaborn.heatmap(mat, square=True, cmap='magma_r')
+
+
+if __name__ == '__main__':
+    CONFIG = load_config()
+    main(CONFIG)
---
+++ scripts/3_plot/od_commodities_maps.py
@@ -0,0 +1,86 @@
+"""Plot commodities maps
+"""
+import os
+
+import cartopy.crs as ccrs
+import geopandas
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+import pandas
+
+
+from oia.utils import load_config, get_axes, plot_basemap, scale_bar, plot_basemap_labels, save_fig
+
+
+def main(config):
+    """Read shapes, plot map
+    """
+    data_path = config['paths']['data']
+    zones_file = os.path.join(data_path, 'boundaries', 'economic_od_zones.shp')
+    od_file = os.path.join(data_path, 'usage', 'economic_od.csv')
+
+    od = pandas.read_csv(od_file)
+    zones = geopandas.read_file(zones_file)
+
+    all_prod = od.groupby(['sector', 'from_zone']).sum()
+    all_attr = od.groupby(['sector', 'to_zone']).sum()
+
+    for sector in od.sector.unique():
+        print(" >", sector)
+
+        productions = all_prod.loc[sector, :]
+        productions = zones.merge(productions, left_on='id', right_on='from_zone')
+        attractions = all_attr.loc[sector, :]
+        attractions = zones.merge(attractions, left_on='id', right_on='to_zone')
+        norm = Normalize(
+            vmin=min(productions.value.min(), attractions.value.min()),
+            vmax=max(productions.value.max(), attractions.value.max())
+        )
+
+        label = "Commodity ({}) productions".format(sector)
+        plot(productions, norm, data_path, label)
+        save_fig(os.path.join(
+            config['paths']['figures'], 'od_{}_orig_map.png'.format(sector)))
+
+        label = "Commodity ({}) attractions".format(sector)
+        plot(attractions, norm, data_path, label)
+        save_fig(os.path.join(
+            config['paths']['figures'], 'od_{}_dest_map.png'.format(sector)))
+
+
+def plot(df, norm, data_path, label):
+    """Plot df
+    """
+    cmap = plt.cm.ScalarMappable(norm=norm, cmap='magma_r')
+    df['color'] = df.value.apply(cmap.to_rgba)
+
+    # basemap
+    proj_lat_lon = ccrs.PlateCarree()
+    ax = get_axes()
+    plot_basemap(ax, data_path)
+    scale_bar(ax, location=(0.8, 0.05))
+    plot_basemap_labels(ax, data_path, include_regions=False)
+
+    # zones
+    for zone in df.itertuples():
+        ax.add_geometries(
+            [zone.geometry],
+            crs=proj_lat_lon,
+            edgecolor='none',
+            facecolor=zone.color,
+            zorder=3
+        )
+
+    cmap.set_array(df.value)
+    cbar = plt.colorbar(
+        cmap, ax=ax, fraction=0.05, pad=0.04, drawedges=False,
+        shrink=0.9, orientation='horizontal'
+    )
+    cbar.outline.set_color("none")
+    cbar.ax.set_xlabel(label)
+
+
+if __name__ == '__main__':
+    CONFIG = load_config()
+    main(CONFIG)
