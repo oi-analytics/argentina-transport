ed123777026f1fd7e5232caf599c51cf101e57c8
itrcrisks <raghavp@gmail.com>
2018-12-06 22:05:45 +0000

    new plot scripts

---
+++ scripts/3_plot/national_hazard_exposure_plots.py
@@ -0,0 +1,225 @@
+"""National hazard exposure maps
+"""
+import os
+import sys
+from collections import OrderedDict
+
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import matplotlib.pyplot as plt
+from shapely.geometry import LineString
+from oia.utils import *
+
+def main():
+    config = load_config()
+
+    data_path = config['paths']['data']
+    modes = ['road', 'rail']
+    hazard_cols = ['hazard_type','climate_scenario','year']
+    plot_set = [
+        {
+            'hazard': 'pluvial flooding',
+            'color': ['#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c','#d9d9d9'],
+            'name': 'Pluvial flooding'
+        },
+        {
+            'hazard': 'fluvial flooding',
+            'color': ['#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c','#d9d9d9'],
+            'name': 'Fluvial flooding'
+        },
+    ]
+    climate_change = False
+    national_pth = os.path.join(config['paths']['output'],
+            'network_stats',
+            'national_scale_hazards_stats.xlsx')
+    
+    # Give the paths to the input data files
+    # load provinces and get geometry of the right province
+    print('* Reading provinces dataframe')
+    province_path = os.path.join(config['paths']['incoming_data'],'2','provincia','Provincias.shp')
+    provinces = gpd.read_file(province_path,encoding='utf-8')
+    provinces = provinces.to_crs({'init': 'epsg:4326'})
+    sindex_provinces = provinces.sindex
+    
+    '''Assign provinces to zones
+    '''
+    print('* Reading department dataframe')
+    zones_path = os.path.join(config['paths']['incoming_data'], '2',
+                                'departamento', 'Departamentos.shp')
+    zones = gpd.read_file(zones_path,encoding='utf-8')
+    zones = zones.to_crs({'init': 'epsg:4326'})
+    zones.rename(columns={'OBJECTID':'department_id','Name':'department_name','Geometry':'geom_type'},inplace=True)
+
+    zones['province_name'] = zones.apply(lambda x: extract_value_from_gdf(
+        x, sindex_provinces, provinces,'nombre'), axis=1)
+    zones['province_id'] = zones.apply(lambda x: extract_value_from_gdf(
+        x, sindex_provinces, provinces,'OBJECTID'), axis=1)
+
+    labels = ['0 to 10', '10 to 20', '20 to 30', '30 to 40', '40 to 100', 'No value']
+    change_colors = ['#1a9850','#66bd63','#a6d96a','#d9ef8b','#fee08b','#fdae61','#f46d43','#d73027','#d9d9d9']
+    change_labels = ['< -40','-40 to -20','-20 to -10','-10 to 0','0 to 10','10 to 20','20 to 40','> 40','No change/value']
+    change_ranges = [(-1e10,-40),(-40,-20),(-20,-10),(-10,0),(0.001,10),(10,20),(20,40),(40,1e10)]
+
+    for mode in modes:
+        all_edge_fail_scenarios = pd.read_excel(national_pth,sheet_name=mode)
+        all_edge_fail_scenarios = all_edge_fail_scenarios[hazard_cols + ['department_id','probability','length','total_length']]
+        all_edge_fail_scenarios = all_edge_fail_scenarios.groupby(hazard_cols + ['department_id','probability'])['length','total_length'].sum().reset_index()
+        all_edge_fail_scenarios['percentage'] = 100.0*all_edge_fail_scenarios['length']/all_edge_fail_scenarios['total_length']
+        all_edge_fail_scenarios = all_edge_fail_scenarios.groupby(hazard_cols + ['department_id'])['percentage'].max().reset_index()
+
+        # Climate change effects
+        if climate_change == True:
+            all_edge_fail_scenarios = all_edge_fail_scenarios.set_index(['hazard_type','department_id'])
+            scenarios = list(set(all_edge_fail_scenarios.index.values.tolist()))
+            change_tup = []
+            for sc in scenarios:
+                perc = all_edge_fail_scenarios.loc[[sc], 'percentage'].values.tolist()
+                yrs = all_edge_fail_scenarios.loc[[sc], 'year'].values.tolist()
+                cl = all_edge_fail_scenarios.loc[[sc], 'climate_scenario'].values.tolist()
+                if 2016 not in yrs:
+                    change_tup += list(zip([sc[0]]*len(cl),[sc[1]]*len(cl),cl,yrs,[1e9]*len(cl)))
+                elif len(cl) > 1:
+                    vals = list(zip(cl,perc,yrs))
+                    vals = sorted(vals, key=lambda pair: pair[-1])
+                    change = np.array([p for (c,p,y) in vals[1:]]) - vals[0][1]
+                    cl = [c for (c,p,y) in vals[1:]]
+                    yrs = [y for (c,p,y) in vals[1:]]
+                    change_tup += list(zip([sc[0]]*len(cl),[sc[1]]*len(cl),cl,yrs,change))
+
+            change_df = pd.DataFrame(change_tup,columns=['hazard_type','department_id','climate_scenario','year','change'])
+            change_df.to_csv(os.path.join(config['paths']['output'],
+                'hazard_scenarios',
+                '{}_climate_change.csv'.format(mode)
+                ), index=False
+            )
+
+            # Change effects
+            change_df = change_df.set_index(hazard_cols)
+            scenarios = list(set(change_df.index.values.tolist()))
+            for sc in scenarios:
+                hazard_type = sc[0]
+                climate_scenario = sc[1]
+                year = sc[2]
+                percentage = change_df.loc[[sc], 'change'].values.tolist()
+                communes = change_df.loc[[sc], 'department_id'].values.tolist()
+                communes_df = pd.DataFrame(list(zip(communes,percentage)),columns=['department_id','change'])
+                commune_vals = pd.merge(region_file,communes_df,how='left',on=['department_id']).fillna(0)
+                del percentage,communes,communes_df
+
+                proj = ccrs.PlateCarree()
+                ax = get_axes()
+                plot_basemap(ax, data_path)
+                scale_bar(ax, location=(0.8, 0.05))
+                plot_basemap_labels(ax, data_path, include_regions=True)
+
+                name = [c['name'] for c in plot_set if c['hazard'] == hazard_type][0]
+                for iter_,record in commune_vals.iterrows():
+                    geom = record.geometry
+                    region_val = record.change
+                    if region_val:
+                        cl = [c for c in range(len((change_ranges))) if region_val >= change_ranges[c][0] and region_val < change_ranges[c][1]]
+                        if cl:
+                            c = cl[0]
+                            ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                            facecolor=change_colors[c], label=change_labels[c])
+                        else:
+                            ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                            facecolor=change_colors[-1], label=change_labels[-1])
+
+                # Legend
+                legend_handles = []
+                for c in range(len(change_colors)):
+                    legend_handles.append(mpatches.Patch(color=change_colors[c], zorder=11,label=change_labels[c]))
+
+                ax.legend(
+                    handles=legend_handles,
+                    title='Percentage change in exposure',
+                    loc='lower right',
+                    fancybox=True,
+                    framealpha=1.0
+                )
+                if climate_scenario == 'none':
+                    climate_scenario = 'current'
+                else:
+                    climate_scenario = climate_scenario.upper()
+                plt.title('Percentage change for {} {} {}'.format(name,climate_scenario,year), fontsize=14)
+                output_file = os.path.join(config['paths']['figures'],
+                                           '{}-{}-{}-{}-exposure-change-percentage.png'.format(mode.replace(' ',''),name,climate_scenario.replace('.',''),year))
+                save_fig(output_file)
+                plt.close()
+
+        # Absolute effects
+        all_edge_fail_scenarios = all_edge_fail_scenarios.reset_index()
+        all_edge_fail_scenarios = all_edge_fail_scenarios.set_index(hazard_cols)
+        scenarios = list(set(all_edge_fail_scenarios.index.values.tolist()))
+        for sc in scenarios:
+            hazard_type = sc[0]
+            climate_scenario = sc[1]
+            year = sc[2]
+            percentage = all_edge_fail_scenarios.loc[[sc], 'percentage'].values.tolist()
+            communes = all_edge_fail_scenarios.loc[[sc], 'department_id'].values.tolist()
+            communes_df = pd.DataFrame(list(zip(communes,percentage)),columns=['department_id','percentage'])
+            commune_vals = pd.merge(zones,communes_df,how='left',on=['department_id']).fillna(0)
+            del percentage,communes,communes_df
+
+            proj = ccrs.PlateCarree()
+            ax = get_axes()
+            plot_basemap(ax, data_path)
+            scale_bar(ax, location=(0.8, 0.05))
+            plot_basemap_labels(ax, data_path, include_regions=True)
+
+            colors = [c['color'] for c in plot_set if c['hazard'] == hazard_type][0]
+            name = [c['name'] for c in plot_set if c['hazard'] == hazard_type][0]
+
+            for iter_,record in commune_vals.iterrows():
+                geom = record.geometry
+                region_val = record.percentage
+                if region_val:
+                    if region_val > 0 and region_val <= 10:
+                        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                          facecolor=colors[0], label=labels[0])
+                    elif region_val > 10 and region_val <= 20:
+                        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                          facecolor=colors[1], label=labels[1])
+                    if region_val > 20 and region_val <= 30:
+                        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                          facecolor=colors[2], label=labels[2])
+                    elif region_val > 30 and region_val <= 40:
+                        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                          facecolor=colors[3], label=labels[3])
+                    elif region_val > 40 and region_val <= 100:
+                        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                          facecolor=colors[4], label=labels[4])
+
+                else:
+                    ax.add_geometries([geom], crs=proj, edgecolor='#ffffff',
+                                      facecolor=colors[5], label=labels[5])
+
+            # Legend
+            legend_handles = []
+            for c in range(len(colors)):
+                legend_handles.append(mpatches.Patch(color=colors[c], label=labels[c]))
+            
+            ax.legend(
+                handles=legend_handles,
+                title='Percentage exposure',
+                loc=(0.6,0.2),
+                fancybox=True,
+                framealpha=1.0
+            )
+            if climate_scenario == 'none':
+                climate_scenario = 'current'
+            else:
+                climate_scenario = climate_scenario.upper()
+            plt.title('Percentage exposure for {} {} {}'.format(name,climate_scenario,year), fontsize=10)
+            output_file = os.path.join(config['paths']['figures'],
+                                       '{}-{}-{}-{}-exposure-percentage.png'.format(mode.replace(' ',''),name,climate_scenario.replace('.',''),year))
+            save_fig(output_file)
+            plt.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/3_plot/rail_network_flows_max_scales.py
@@ -0,0 +1,168 @@
+"""Road network flow maps
+"""
+import os
+import sys
+from collections import OrderedDict
+
+import geopandas as gpd
+import pandas as pd
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import matplotlib.pyplot as plt
+from shapely.geometry import LineString
+from oia.utils import *
+
+
+def main():
+    config = load_config()
+    data_path = config['paths']['data']
+    mode_file_path = os.path.join(config['paths']['data'], 'network',
+                                   'rail_edges.shp')
+    flow_file_path = os.path.join(config['paths']['output'], 'flow_mapping_combined',
+                                   'weighted_flows_rail_100_percent.csv')
+
+
+    mode_file = gpd.read_file(mode_file_path,encoding='utf-8')
+    flow_file = pd.read_csv(flow_file_path)
+    mode_file = pd.merge(mode_file,flow_file,how='left', on=['edge_id']).fillna(0)
+
+    flow_color = '#006d2c'
+    no_flow_color = '#636363'
+    plot_sets = [
+        {
+            'file_tag': 'commodities',
+            'legend_label': "AADF ('000 tons/day)",
+            'divisor': 1000,
+            'columns': ['max_total_tons','max_AGRICULTURA, GANADERÍA, CAZA Y SILVICULTURA', 
+                        'max_COMERCIO', 'max_EXPLOTACIÓN DE MINAS Y CANTERAS', 
+                        'max_INDUSTRIA MANUFACTURERA', 'max_TRANSPORTE Y COMUNICACIONES' 
+                        ],
+            'title_cols': ['Total tonnage','AGRICULTURA, GANADERÍA, CAZA Y SILVICULTURA',
+                    'COMERCIO','EXPLOTACIÓN DE MINAS Y CANTERAS',
+                    'INDUSTRIA MANUFACTURERA','TRANSPORTE Y COMUNICACIONES'
+                    ],
+            'significance':0
+        },
+    ]
+
+    styles = OrderedDict([
+                ('1',  Style(color='#006d2c', zindex=6, label='Flow')),
+                ('2', Style(color='#636363', zindex=8, label='No flow')),
+            ])
+    for plot_set in plot_sets:
+        for c in range(len(plot_set['columns'])):
+            # basemap
+            proj_lat_lon = ccrs.PlateCarree()
+            ax = get_axes()
+            plot_basemap(ax, data_path)
+            scale_bar(ax, location=(0.8, 0.05))
+            plot_basemap_labels(ax, data_path, include_regions=False)
+
+            # generate weight bins
+            column = plot_set['columns'][c]
+            weights = [
+                record['max_total_tons']
+                for iter_, record in mode_file.iterrows() if record['max_total_tons'] > 0
+            ]
+            max_weight = max(weights)
+            width_by_range = generate_weight_bins(weights, n_steps=9, width_step=0.015, interpolation='log')
+
+            geoms_by_range = {}
+            for value_range in width_by_range:
+                geoms_by_range[value_range] = []
+
+            for iter_, record in mode_file.iterrows():
+                val = record[column]
+                geom = record.geometry
+                if val > 0:
+                    for nmin, nmax in geoms_by_range:
+                        if nmin <= val and val < nmax:
+                            geoms_by_range[(nmin, nmax)].append(geom)
+                else:
+                    ax.add_geometries(
+                    [geom],
+                    crs=proj_lat_lon,
+                    linewidth=0.5,
+                    edgecolor=no_flow_color,
+                    facecolor='none',
+                    zorder=1)
+
+
+                        # plot
+            for range_, width in width_by_range.items():
+                ax.add_geometries(
+                    [geom.buffer(width) for geom in geoms_by_range[range_]],
+                    crs=proj_lat_lon,
+                    edgecolor='none',
+                    facecolor=flow_color,
+                    zorder=2)
+
+            x_l = -62.4
+            x_r = x_l + 0.4
+            base_y = -42.1
+            y_step = 0.8
+            y_text_nudge = 0.2
+            x_text_nudge = 0.2
+
+            ax.text(
+                x_l,
+                base_y + y_step - y_text_nudge,
+                plot_set['legend_label'],
+                horizontalalignment='left',
+                transform=proj_lat_lon,
+                size=10)
+
+            divisor = plot_set['divisor']
+            significance_ndigits = plot_set['significance']
+            max_sig = []
+            for (i, ((nmin, nmax), line_style)) in enumerate(width_by_range.items()):
+                if round(nmin/divisor, significance_ndigits) < round(nmax/divisor, significance_ndigits):
+                    max_sig.append(significance_ndigits)
+                elif round(nmin/divisor, significance_ndigits+1) < round(nmax/divisor, significance_ndigits+1):
+                    max_sig.append(significance_ndigits+1)
+                elif round(nmin/divisor, significance_ndigits+2) < round(nmax/divisor, significance_ndigits+2):
+                    max_sig.append(significance_ndigits+2)
+                else:
+                    max_sig.append(significance_ndigits+3)
+
+            significance_ndigits = max(max_sig)
+
+            for (i, ((nmin, nmax), width)) in enumerate(width_by_range.items()):
+                y = base_y - (i*y_step)
+                line = LineString([(x_l, y), (x_r, y)]).buffer(width)
+                ax.add_geometries(
+                    [line],
+                    crs=proj_lat_lon,
+                    linewidth=0,
+                    edgecolor='#000000',
+                    facecolor='#000000',
+                    zorder=2)
+                if abs(nmin - max_weight) < 1e-5:
+                    value_template = '>{:.' + str(significance_ndigits) + 'f}'
+                    label = value_template.format(
+                        round(max_weight/divisor, significance_ndigits))
+                else:
+                    value_template = '{:.' + str(significance_ndigits) + \
+                        'f}-{:.' + str(significance_ndigits) + 'f}'
+                    label = value_template.format(
+                        round(nmin/divisor, significance_ndigits), round(nmax/divisor, significance_ndigits))
+
+                ax.text(
+                    x_r + x_text_nudge,
+                    y - y_text_nudge,
+                    label,
+                    horizontalalignment='left',
+                    transform=proj_lat_lon,
+                    size=10)
+
+            plt.title('Max AADF - {}'.format(plot_set['title_cols'][c]), fontsize=10)
+            legend_from_style_spec(ax, styles)
+            output_file = os.path.join(
+                config['paths']['figures'],
+                'rail_flow-map-{}-{}-max-scale.png'.format(plot_set['file_tag'], column))
+            save_fig(output_file)
+            plt.close()
+
+
+if __name__ == '__main__':
+    main()
