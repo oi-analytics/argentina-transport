0cc14aec210029e0dcb36f305ebaa9c2de0202c7
Tom Russell <tomalrussell@gmail.com>
2019-05-10 09:11:06 +0100

    Scaffold docs

--- setup.py
+++ setup.py
@@ -1,65 +1,18 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-"""Setup packages in src directory to be accessible to scripts
+# -*- coding: utf-8 -*-
+"""Setup file for atra.
 """
-from __future__ import absolute_import
-from __future__ import print_function
-
-import io
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
-
-from setuptools import find_packages
+import sys
 from setuptools import setup
 
 
-def read(*names, **kwargs):
-    """Read file contents
-    """
-    return io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ).read()
+def setup_package():
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
+          entry_points={},
+          use_pyscaffold=True)
 
 
-setup(
-    name='oia',
-    version='0.1.0',
-    license='MIT License',
-    description='Library package developed alongside OIA Argentina Transport project',
-    long_description='%s\n' % read('README.md'),
-    author='Oxford Infrastructure Analytics',
-    author_email='enquiries@oi-analytics.com',
-    url='https://github.com/oi-analytics/argentina-transport',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
-    ],
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
-    install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
-    entry_points={
-        # eg:
-        # 'console_scripts': [
-        #     'nameless = nameless.cli:main',
-        # ]
-    },
-)
+if __name__ == "__main__":
+    setup_package()
